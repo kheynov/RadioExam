99c3e567b6675174615125819dea751dd5badbcf
Vladislav Kheynov <lcastory86@gmail.com>
2023-11-15 18:41:08 +0700

    fix: dictionary

--- app/src/main/java/ru/kheynov/radioexam/screens/study/StudyScreen.kt
+++ app/src/main/java/ru/kheynov/radioexam/screens/study/StudyScreen.kt
@@ -6,7 +6,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -31,98 +32,103 @@ fun StudyScreen(
 ) {
     val question = viewModel.currentQuestion.observeAsState()
 
-    LazyColumn(Modifier
-        .fillMaxWidth()
-        .padding(horizontal = 16.dp, vertical = 16.dp),
+    Column(
+        Modifier
+            .fillMaxWidth()
+            .verticalScroll(rememberScrollState())
+            .padding(horizontal = 16.dp, vertical = 16.dp),
         horizontalAlignment = Alignment.Start
     ) {
-        item {
-            Row(
-                modifier = Modifier.padding(bottom = 8.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(text = "Вопрос №${question.value?.id}",
-                    modifier = Modifier
-                        .weight(8f),
-                    style = MaterialTheme.typography.h1
-                )
-                Text(text = "${viewModel.questionNumber.first} из ${
+        Row(
+            modifier = Modifier.padding(bottom = 8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Вопрос №${question.value?.id}",
+                modifier = Modifier
+                    .weight(8f),
+                style = MaterialTheme.typography.h1
+            )
+            Text(
+                text = "${viewModel.questionNumber.first} из ${
                     viewModel.questionNumber
                         .second
                 }",
-                    modifier = Modifier
-                        .weight(5f),
-                    style = MaterialTheme.typography.h3,
-                    textAlign = TextAlign.End)
-            }
-            Text(
-                text = question.value!!.text,
-                style = MaterialTheme.typography.h2
+                modifier = Modifier
+                    .weight(5f),
+                style = MaterialTheme.typography.h3,
+                textAlign = TextAlign.End
             )
-            question.value!!.image_id?.let {
-                GlideImage(
-                    modifier = Modifier.padding(top = 16.dp),
-                    imageModel = "android.resource://ru.kheynov.radioexam/drawable/exam$it",
-                    contentScale = ContentScale.FillWidth,
-                )
-            }
+        }
+        Text(
+            text = question.value!!.text,
+            style = MaterialTheme.typography.h2
+        )
+        question.value!!.image_id?.let {
+            GlideImage(
+                modifier = Modifier.padding(top = 16.dp),
+                imageModel = "android.resource://ru.kheynov.radioexam/drawable/exam$it",
+                contentScale = ContentScale.FillWidth,
+            )
+        }
 
-            Column(modifier = Modifier
+        Column(
+            modifier = Modifier
                 .fillMaxWidth()
                 .padding(horizontal = 8.dp, vertical = 16.dp)
-            ) {
-                question.value?.let {
-                    Text(
-                        text = "1) ${it.var1}",
-                        color = if (it.correct == 1)
-                            Color.Red else Color.Black,
-                        modifier = Modifier.padding(bottom = 8.dp),
-                        fontSize = 18.sp,
-                    )
-                    Text(
-                        text = "2) ${it.var2}",
-                        color = if (it.correct == 2)
-                            Color.Red else Color.Black,
-                        modifier = Modifier.padding(bottom = 8.dp),
-                        fontSize = 18.sp,
-                    )
-                    Text(
-                        text = "3) ${it.var3}",
-                        color = if (it.correct == 3)
-                            Color.Red else Color.Black,
-                        modifier = Modifier.padding(bottom = 8.dp),
-                        fontSize = 18.sp,
-                    )
-                    Text(
-                        text = "4) ${it.var4}",
-                        color = if (it.correct == 4)
-                            Color.Red else Color.Black,
-                        modifier = Modifier.padding(bottom = 8.dp),
-                        fontSize = 18.sp,
-                    )
+        ) {
+            question.value?.let {
+                Text(
+                    text = "1) ${it.var1}",
+                    color = if (it.correct == 1)
+                        Color.Red else Color.Black,
+                    modifier = Modifier.padding(bottom = 8.dp),
+                    fontSize = 18.sp,
+                )
+                Text(
+                    text = "2) ${it.var2}",
+                    color = if (it.correct == 2)
+                        Color.Red else Color.Black,
+                    modifier = Modifier.padding(bottom = 8.dp),
+                    fontSize = 18.sp,
+                )
+                Text(
+                    text = "3) ${it.var3}",
+                    color = if (it.correct == 3)
+                        Color.Red else Color.Black,
+                    modifier = Modifier.padding(bottom = 8.dp),
+                    fontSize = 18.sp,
+                )
+                Text(
+                    text = "4) ${it.var4}",
+                    color = if (it.correct == 4)
+                        Color.Red else Color.Black,
+                    modifier = Modifier.padding(bottom = 8.dp),
+                    fontSize = 18.sp,
+                )
+            }
+        }
+
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.BottomCenter,
+        ) {
+            Row(modifier = Modifier.padding(horizontal = 8.dp)) {
+                Button(modifier = Modifier
+                    .weight(1f)
+                    .padding(horizontal = 8.dp, vertical = 8.dp),
+                    onClick = { viewModel.previousQuestion() }) {
+                    Text(text = "Назад")
+                }
+                Button(modifier = Modifier
+                    .weight(1f)
+                    .padding(horizontal = 8.dp, vertical = 8.dp),
+                    onClick = { viewModel.nextQuestion() }) {
+                    Text(text = "Далее")
                 }
             }
         }
     }
 
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.BottomCenter,
-    ) {
-        Row(modifier = Modifier.padding(horizontal = 8.dp)) {
-            Button(modifier = Modifier
-                .weight(1f)
-                .padding(horizontal = 8.dp, vertical = 8.dp),
-                onClick = { viewModel.previousQuestion() }) {
-                Text(text = "Назад")
-            }
-            Button(modifier = Modifier
-                .weight(1f)
-                .padding(horizontal = 8.dp, vertical = 8.dp),
-                onClick = { viewModel.nextQuestion() }) {
-                Text(text = "Далее")
-            }
-        }
-    }
 
 }
